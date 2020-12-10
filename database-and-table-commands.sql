0933b48383a485229ecbd8337adc40938d7fb1fb
Thomas Sobor <sobor.thomas@yahoo.com>
2020-12-09 17:01:10 -0800

    'add create table information'

--- database-and-table-commands.sql
+++ database-and-table-commands.sql
@@ -20,6 +20,20 @@ CREATE TABLE <name>
         column_name data_type
     );
 
+-- Create a table where a cell cannot be null
+CREATE TABLE <name>
+    (
+        column_name data_type NOT NULL,
+        column_name data_type NOT NULL
+    );
+
+-- Create a table with default values
+CREATE TABLE <name>
+    (
+        column_name data_type DEFAULT <default_data>,
+        column_name data_type DEFAULT <default_data>
+    );
+
 -- Show all tables in a database (cli command)
 SHOW TABLES;
 
