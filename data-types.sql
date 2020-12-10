9612b93fc968f18d12081c7702553178fb31af90
Thomas Sobor <sobor.thomas@yahoo.com>
2020-12-09 16:25:04 -0800

    first commit

---
+++ data-types.sql
@@ -0,0 +1,4 @@
+/*
+    INT         - Whole Numbers
+    CHAR        - Fixed-Length String
+    VARCHAR     - Variable-Length String (between 1 and 255 characters)
---
+++ database-and-table-commands.sql
@@ -0,0 +1,32 @@
+-- Show all databases (cli command)
+show databases;
+
+-- Create a database
+CREATE DATABASE <name>;
+
+-- Deleting a database
+DROP DATABASE <name>;
+
+-- Use a database
+USE <database name>;
+
+-- Check which database you are currently in
+SELECT database();
+
+-- Create a table
+CREATE TABLE <name>
+    (
+        column_name data_type,
+        column_name data_type
+    );
+
+-- Show all tables in a database (cli command)
+SHOW TABLES;
+
+-- Show the columns from a particular table
+SHOW COLUMNS FROM <tablename>;
+-- or
+DESC <tablename>;
+
+-- Deleting Tables
+
