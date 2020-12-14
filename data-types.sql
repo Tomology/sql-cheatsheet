e11757862c4f2efc4398e641911fb60b79c09d62
Thomas Sobor <sobor.thomas@yahoo.com>
2020-12-14 11:57:43 -0800

    update data types

--- data-types.sql
+++ data-types.sql
@@ -1,7 +1,19 @@
 /*
     INT         - Whole Numbers
+    DECIMAL     - Decmial Numbers
+                - Syntax is:
+                - DECIMAL(total number of digits, digits after decimal)
+    FLOAT       - Decimal Numbers. Will store larger numbers using less
+                - space but will be less precise. Will have precision
+                - issues after approx 7 digits. 4 bytes needed.
+    DOUBLE      - Same as FLOAT but will have precision issues after
+                - approx 15 digits. 8 bytes needed.
     CHAR        - Fixed-Length String
-    VARCHAR     - Variable-Length String (between 1 and 255 characters)
+    VARCHAR     - Variable-Length String (between 0 and 255 characters).
+    DATE        - YYYY-MM-DD, date with no time
+    TIME        - HH:MM:SS, time with no date
+    DATETIME    - YYYY-MM-DD HH:MM:SS, Date and Time
+
 
 
     NULL        - The value is not known
